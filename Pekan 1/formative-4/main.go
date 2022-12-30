63df0dad086d6968c997ede81f81664cde167f77
RaihanRafif <raihanrafif200095@gmail.com>
2022-12-30 08:56:46 +0700

    tugas d-4

---
+++ Pekan 1/formative-4/main.go
@@ -0,0 +1,59 @@
+package main
+
+func main() {
+	//SOAL 1
+	// for i := 1; i <= 20; i++ {
+	// 	if i%2 != 0 {
+	// 		if i%3 == 0 {
+	// 			fmt.Println(i, " - I Love Coding")
+	// 		} else {
+	// 			fmt.Println(i, " - Santai")
+	// 		}
+	// 	} else {
+	// 		fmt.Println(i, " - Berkualitas")
+	// 	}
+	// }
+
+	//SOAL 2
+	// for i := 0; i <= 6; i++ {
+	// 	fmt.Println()
+	// 	for j := 0; j <= i; j++ {
+	// 		fmt.Printf("#")
+	// 	}
+	// }
+
+	//SOAL 3
+	// var kalimat = [...]string{"aku", "dan", "saya", "sangat", "senang", "belajar", "golang"}
+	// for i := 2; i < len(kalimat); i++ {
+	// 	fmt.Print(kalimat[i], " ")
+	// }
+
+	//SOAL 4
+	// var sayuran = []string{}
+	// var items = []string{
+	// 	"Bayam",
+	// 	"Buncis",
+	// 	"Kangkung",
+	// 	"Kubis",
+	// 	"Seledri",
+	// 	"Tauge",
+	// 	"Timun",
+	// }
+	// sayuran = append(sayuran, items...)
+	// for i := 1; i < len(sayuran)+1; i++ {
+	// 	fmt.Println(i, "."+sayuran[i-1])
+	// }
+
+	//SOAL 5
+	// var satuan = map[string]int{
+	// 	"panjang": 7,
+	// 	"lebar":   4,
+	// 	"tinggi":  6,
+	// }
+
+	// for satuan, nilai := range satuan {
+	// 	fmt.Println(satuan, " = ", nilai)
+	// }
+	// fmt.Println("Volume Balok = ", satuan["panjang"]*satuan["lebar"]*satuan["tinggi"])
+
+}
